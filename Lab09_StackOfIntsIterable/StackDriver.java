9b119c16629655ee3c22a10b6e835ed558d1961b
Youngsup Kim <idebtor@gmail.com>
2018-11-15 07:46:07 +0900

    Update StackDriver.java

--- Lab09_StackOfIntsIterable/StackDriver.java
+++ Lab09_StackOfIntsIterable/StackDriver.java
@@ -2,8 +2,8 @@
 // Warming-up program for java.util.Stack
 public class StackDriver {
 	public static void main(String[] args) {
-		// java.util.Stack<Integer> s = new java.util.Stack<>();
-		StackOfInts s = new StackOfInts();
+		java.util.Stack<Integer> s = new java.util.Stack<>();
+		// StackOfInts s = new StackOfInts();
 		System.out.println("size:" + s.size() + "  capacity:" + s.capacity());
 		s.push(7);
 		s.push(4);
