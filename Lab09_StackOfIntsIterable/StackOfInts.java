a4ba54edb49c2ee5b30d1904c113633bc74f0e0d
Youngsup Kim <idebtor@gmail.com>
2018-11-15 07:59:54 +0900

    updated and initial commit

---
+++ Lab09_StackOfIntsIterable/StackOfInts.java
@@ -0,0 +1,49 @@
+public class StackOfInts {
+	private int[] elements;
+	private int size;
+	private static final int DEFAULT_CAPACITY = 10;
+
+	/** Construct a stack with the default capacity */
+	public StackOfInts() {
+		this(DEFAULT_CAPACITY);
+	}
+	/** Construct a stack with the specified capacity */
+	public StackOfInts(int capacity) {
+		elements = new int[capacity];
+	}
+
+	/** Push a new integer into the top of the stack */
+	public void push(int item) {
+		// Your code here
+	}
+
+	/** Return and remove the top element from the stack */
+	public int pop() {
+		return elements[--size];
+	}
+
+	/** Return the top element from the stack */
+	public int peek() {
+		return elements[size() - 1];
+	}
+
+	/** Return true if the stack is empty */
+	public boolean empty() {
+		return size == 0;
+	}
+
+	/** Return the number of elements in the stack */
+	public int size() {
+		return size;
+	}
+
+   /** Return the capacity of the stack */
+	public int capacity() {
+		return elements.length;
+	}
+
+	/** Make the stack empty */
+	public int clear() {
+       return 0;  // Your code here
+  }
+}
